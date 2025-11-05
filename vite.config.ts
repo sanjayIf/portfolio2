0dc1051ecff567a7667cddfaeef158fddfd6425e
sanjayIf <157701633+sanjayIf@users.noreply.github.com>
2025-11-06 02:52:01 +0530

    Setup GitHub Pages for portfolio2

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // 👇 this must match your GitHub repo name
+  base: "/portfolio2/",
+
   server: {
     host: "::",
-    port: 8080,
+    port: 8088,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
